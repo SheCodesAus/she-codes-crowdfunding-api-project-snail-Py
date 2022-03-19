d44cc0a9893fd9f026d6219ce204588209e758d4
snail-Py <cs.legoff@gmail.com>
2022-03-19 13:05:37 +0800

    Worked on permissions and authentication

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ super user
 username = user
 email = user@example.com
 pass = user
+
+username = user2
+email = user2@example.com
+pass = user2
--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,10 +1,10 @@
-# from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Project, Pledge
 from. serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, permissions
+from .permissions import IsOwnerorReadOnly
 
 class PledgeList(APIView):
     def get(self, request):
@@ -25,6 +25,10 @@ class PledgeList(APIView):
             status=status.HTTP_400_BAD_REQUEST)
 
 class ProjectList(APIView):
+    
+    # I can see the project list when loged off but I can't post/create a project unless logged in.
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
     def get(self, request):
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
@@ -43,10 +47,17 @@ class ProjectList(APIView):
             status=status.HTTP_400_BAD_REQUEST)
 
 class ProjectDetail(APIView):
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerorReadOnly]
+
     def get_object(self, pk):
         # return Project.objects.get(pk=pk)
         try:
-            return Project.objects.get(pk=pk)
+            # return Project.objects.get(pk=pk)
+            project = Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request,project)
+            return project
+
         except Project.DoesNotExist:
             raise Http404
     
@@ -54,5 +65,18 @@ class ProjectDetail(APIView):
         project = self.get_object(pk)
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        data = request.data
+        serializer = ProjectDetailSerializer(
+            instance = project,
+            data = data,
+            partial = True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
